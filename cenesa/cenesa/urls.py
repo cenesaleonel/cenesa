0b2b414d71079bba874e2a39a315e53c0e0eb747
Pescador Leonel <leopescador07@gmail.com>
2024-09-18 00:52:13 -0300

    comiezo de proyecto

--- cenesa/cenesa/urls.py
+++ cenesa/cenesa/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for cenesa_formularios project.
+URL configuration for cenesa project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  
+from django.contrib.auth import views as auth_views
+from formularios import views 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),  
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),  # Ruta para login
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),  # Ruta para logout
+    path('', views.home, name='home'),  
 ]
---
+++ cenesa/formularios/admin.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponseForbidden
+
+def alguna_vista(request):
+    if request.user.tipo_usuario != 'admin':
+        return HttpResponseForbidden("No tienes permiso para acceder a esta página.")
+    # Lógica para usuarios admin...
